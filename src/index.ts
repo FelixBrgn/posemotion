7233744aa7d02f6a8ad3ca2826a0396732df15cd
Felix Brüggemann <f.brgn001@gmail.com>
2021-02-26 12:32:29 +0100

    Added DetectionBodyRotation to Index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { PosenetParts } from './detection/detection';
 import { DetectionAngle, AngleMetaData } from './detection/detectionAngle';
+import { DetectionBodyRotation } from './detection/detectionBodyRotation';
 
 // Detection 
 export { PosenetParts };
 
 // Angle
 export { DetectionAngle };
+export { DetectionBodyRotation };
 export { AngleMetaData };
