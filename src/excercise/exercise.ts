55d3652c7adfcf53fa7d169f634897fef9c89f62
Felix Brüggemann <f.brgn001@gmail.com>
2021-03-03 16:19:47 +0100

    Change functionality of exercise

--- src/excercise/exercise.ts
+++ src/excercise/exercise.ts
@@ -3,39 +3,5 @@ import { IDetection } from '../detection/detection';
 export interface IExercise {
     _detections: IDetection[];
     _sequence: string[];
-    next(output: any): string[]
-}
-
-export class Exercise implements IExercise {
-    _detections: IDetection[];
-    _sequence: string[];
-    constructor(detections: IDetection[], sequence: string[]) {
-        this._detections = detections;
-        this._sequence = sequence;
-    }
-
-    next(output: any): string[] {
-        let res: string[] = [];
-        this._detections.forEach(detection => {
-
-            let states: string[] = detection.next(output);
-            if (states == []) {
-                return [];
-            }
-            if (res.length == 0) {
-                res = states;
-            }
-            else {
-                let sameStates: string[] = [];
-                states.forEach(state => {
-                    if (res.find(e => e == state)) {
-                        sameStates.push(state);
-                    }
-                });
-                res = sameStates;
-            }
-
-        });
-        return res!;
-    }
+    next(output: any): boolean
 }
---
+++ src/excercise/exerciseRepetitions.ts
@@ -0,0 +1,47 @@
+import { IDetection } from '../detection/detection';
+import { IExercise } from './exercise';
+
+export class Exercise implements IExercise {
+    _detections: IDetection[];
+    _sequence: string[];
+    _neededStatusIndex: number = 0;
+    constructor(detections: IDetection[], sequence: string[]) {
+        this._detections = detections;
+        this._sequence = sequence;
+    }
+
+    next(output: any): boolean {
+        let res: string[] = [];
+        this._detections.forEach(detection => {
+            let states: string[] = detection.next(output);
+            if (states == []) {
+                return [];
+            }
+            if (res.length == 0) {
+                res = states;
+            }
+            else {
+                let sameStates: string[] = [];
+                states.forEach(state => {
+                    if (res.find(e => e == state)) {
+                        sameStates.push(state);
+                    }
+                });
+                res = sameStates;
+            }
+        });
+
+        return this.checkIfNeededStatus(res);
+    }
+
+    checkIfNeededStatus(currentStatus: string[]): boolean {
+        if (currentStatus.find(x => x == this._sequence[this._neededStatusIndex])) {
+            this._neededStatusIndex++;
+            if (this._neededStatusIndex >= this._sequence.length) {
+                this._neededStatusIndex = 0;
+                return true;
+            }
+        }
+        return false
+    }
+}
